8b42ff9f8b4f3a044c0a79a34cf854679ab31915
LAPTOP-HDN1ODN0\mizuki.mizuki.0331 <mizuki.mizuki.0331@docomo.ne.jp>
2017-09-06 09:31:08 +0900

    no message

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 function answer(){
     var input1 = document.getElementById('input1');
     var input2 = document.getElementById('input2');
+    var input3 = document.getElementById('input3');
     var output = document.getElementById('output');
 
     //文字として結合
     //var result = input1.value + input2.value;
 
     //整数として足し算
-    var result = parseInt(input1.value) + parseInt(input2.value);
+    var result = parseInt(input1.value) / parseInt(input2.value) / parseInt(input3.value);
 
     output.textContent = result
 }
